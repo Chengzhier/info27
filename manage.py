62833be8e8124ae98e03692947bd44d08c102ee1
chengweiping <810457627@qq.com>
2018-10-28 00:13:20 +0800

    集成日志到项目

--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
-from flask import session
+from flask import session, current_app
+from flask import logging
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from info import create_app, db
 
 # 通过制定的配置名字创建对应配置app
 # create_app 就类似于工厂方法
-app= create_app('development')
+app = create_app('development')
 manager = Manager(app)
 # 將app于db关联
 Migrate(app, db)
@@ -15,7 +16,14 @@ manager.add_command('db', MigrateCommand)
 
 @app.route('/')
 def index():
-    session["name"] = "baidu.com"
+    # session["name"] = "baidu.com"
+    # 测试打印日志
+    logging.debug("测试debug")
+    logging.warning("测试warning")
+    logging.error("测试error")
+    logging.fatal("测试fatal")
+    #根据项目应用logger 会根据应用程序的调试状态去调整日志级别
+    current_app.logger.error("测试error")
     return 'index'
 
 
